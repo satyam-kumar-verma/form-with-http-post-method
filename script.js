4bccfff2c0091de0671e31886d16e261e8fdd0d9
satyam-kumar-verma <billionairedolike@gmail.com>
2023-10-07 12:18:11 +0530

    save

---
+++ script.js
@@ -0,0 +1,114 @@
+let formEl = document.getElementById("addUserForm");
+
+let nameEl = document.getElementById("name");
+let emailEl = document.getElementById("email");
+
+let nameErrMsg = document.getElementById("nameErr");
+let emailErrMsg = document.getElementById("emailErr");
+
+let statusEl = document.getElementById("status");
+let genderM = document.getElementById("genderM");
+let genderF = document.getElementById("genderF");
+
+let resultEl = document.getElementById("resultEl");
+
+let formData = {
+    name:"",
+    email:"",
+    gender:"Male",
+    status:"Active"
+}
+
+function getNameDetails(event){
+    let inputName = event.target.value;
+    formData.name = inputName;
+}
+
+function getEmailDetails(event){
+    let inputEmail = event.target.value;
+    formData.email = inputEmail;
+}
+
+function getStatusDetails(event){
+    formData.status = event.target.value;
+}
+
+function getGenderDetails(event){
+    formData.gender = event.target.value;
+}
+
+function validateForm(){
+    if(nameEl.value === ""){
+        nameErrMsg.textContent = "Required*";
+    }
+    else{
+        nameErrMsg.textContent = "";
+    }
+
+    if(emailEl.value === ""){
+        emailErrMsg.textContent = "Required*";
+    }
+    else{
+        emailErrMsg.textContent = "";
+    }
+}
+
+function fetchData(){
+    resultEl.textContent = "";
+
+    let url = "https://gorest.co.in/public-api/users";
+    let options = {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            Accept:"application/json",
+            Authorization:"Bearer 3bbe64909f74383193384911a3d817cc10c5fae71c13a9bca3f4db4f1bb880a1"
+        },
+        body:JSON.stringify(formData)
+    }
+
+    fetch(url, options)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(jsonData){
+
+        console.log(jsonData);
+
+        if(jsonData.code === 422){
+            if(jsonData.data[0].message === "has already been taken"){
+                emailErrMsg.textContent = "Email already taken";
+
+                resultEl.textContent = "Failed";
+                resultEl.classList.remove("result-style");
+                resultEl.classList.add("err-msg");
+            }
+        }
+
+        if(jsonData.code === 201){
+            resultEl.textContent = "success";
+            resultEl.classList.remove("err-msg");
+            resultEl.classList.add("result-style");
+        }
+        else{
+            resultEl.textContent = "Failed";
+            resultEl.classList.remove("result-style");
+            resultEl.classList.add("err-msg");
+        }
+    })
+
+    
+}
+
+function finalSubmission(event){
+    event.preventDefault();
+    validateForm();
+    fetchData();
+}
+
+nameEl.addEventListener("change",getNameDetails);
+emailEl.addEventListener("change",getEmailDetails);
+statusEl.addEventListener("change",getStatusDetails);
+genderM.addEventListener("change",getGenderDetails);
+genderF.addEventListener("change",getGenderDetails);
+formEl.addEventListener("submit",finalSubmission);
